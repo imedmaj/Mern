3bf3c37df39468237458628990c87de9ff3c79c7
unknown <imed.majouri@outlook.com>
2024-07-27 23:46:08 +0100

    DATE27/07/2024-6

--- React/Prop It Up/prop-it-up/src/components/PersonCard.jsx
+++ React/Prop It Up/prop-it-up/src/components/PersonCard.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+
 
 const PersonCard = (props) => {
-    const { firstName, age,color } = props;
+    const {firstName, age,color } = props;
+    const [number, setnumber] = useState(props.age);
+    const increm =()=>
+    {
+        setnumber(number+1)
+    }
+  
+   
+
     return (
         <div>
             <h1>
                {props.firstName} 
             </h1>
-                       <h2>age: {age} </h2>
+                       <h2>age:{number}  </h2>
             <h3>hair color {color}</h3>
+            <button onClick={increm}>click me</button>
+          
+
         </div>
     );
 }
